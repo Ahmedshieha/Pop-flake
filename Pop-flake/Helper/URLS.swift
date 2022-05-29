94c631321551586347cc30067d8c7caeddb3d97b
AhmedReda <Ahmedshieha@icloud.com>
2022-05-30 01:36:48 +0200

    add urls api enum

---
+++ Pop-flake/Helper/URLS.swift
@@ -0,0 +1,15 @@
+//
+//  URLS.swift
+//  Pop-flake
+//
+//  Created by MacBook on 30/05/2022.
+//
+
+import Foundation
+
+
+enum UrlsApi : String {
+    case topMovieApi = "https://imdb-api.com/en/API/Top250Movies/k_77btgszr"
+    case commingSoon = "https://imdb-api.com/en/API/ComingSoon/k_77btgszr"
+    case InInTheaters = "https://imdb-api.com/en/API/InTheaters/k_77btgszr"
+}
---
+++ Pop-flake/ViewModel/Parser.swift
@@ -0,0 +1,15 @@
+//
+//  Parser.swift
+//  Pop-flake
+//
+//  Created by MacBook on 30/05/2022.
+//
+
+import Foundation
+
+
+struct Parser {
+    
+    
+    
+}
