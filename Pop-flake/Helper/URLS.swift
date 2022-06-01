0ed4106acb43e6aa9f8f0809c47f09339454f44e
AhmedReda <Ahmedshieha@icloud.com>
2022-06-01 21:45:46 +0200

    change api key

--- Pop-flake/Helper/URLS.swift
+++ Pop-flake/Helper/URLS.swift
@@ -9,9 +9,9 @@ import Foundation
 
 
 enum UrlsApi : String {
-    case topMovieApi = "https://imdb-api.com/en/API/Top250Movies/k_50mlyq15"
-    case commingSoon = "https://imdb-api.com/en/API/ComingSoon/k_50mlyq15"
-    case InInTheaters = "https://imdb-api.com/en/API/InTheaters/k_50mlyq15"
-    case topBoxing = "https://imdb-api.com/en/API/BoxOffice/k_50mlyq15"
-    case serachApi = "https://imdb-api.com/en/API/SearchMovie/k_50mlyq15/"
+    case topMovieApi = "https://imdb-api.com/en/API/Top250Movies/k_77btgszr"
+    case commingSoon = "https://imdb-api.com/en/API/ComingSoon/k_77btgszr"
+    case InInTheaters = "https://imdb-api.com/en/API/InTheaters/k_77btgszr"
+    case topBoxing = "https://imdb-api.com/en/API/BoxOffice/k_77btgszr"
+    case serachApi = "https://imdb-api.com/en/API/SearchMovie/k_77btgszr/"
 }
